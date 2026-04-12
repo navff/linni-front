b9106eaaa717681370e040e10e4efdbd123bf459
Vladimir Varnavskiy <var@33kita.ru>
2026-04-12 16:01:24 +0300

    Борьба за тайминг, ч.1

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 import './index.css';
 
+fetch(`${import.meta.env.VITE_API_URL}/health`).catch(() => {});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
